4423a6096d045078789b51421662ac9a47c634c4
Charmi <charmisharma577@gmail.com>
2017-08-04 10:11:31 +0530

    spy detials updated

---
+++ spy_detials.py
@@ -0,0 +1,6 @@
+print "let's get started"
+spy_name = raw_input("enter your name :")
+spy_salutation = raw_input("enter your salution :")
+#concatination of name and salutation
+spy_name = spy_salutation + " " + spy_name
+print "Welcome " + spy_name + " glad to have you back with us"
