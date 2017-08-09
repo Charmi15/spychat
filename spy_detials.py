ab7957633e07191391848e83aaf273d56e1e8c5a
Charmi <charmisharma577@gmail.com>
2017-08-09 20:23:51 +0530

    files uploaded

--- spy_detials.py
+++ spy_detials.py
@@ -1,14 +1,34 @@
-print "let's get started"
-spy_name = raw_input("enter your name :")
-spy_salutation = raw_input("enter your salution :")
-#concatination of name and salutation
-if len(spy_name)>0:
-    spy_age=0
-    spy_rating=0.0
-    spy_is_online=False
-    spy_age=int(raw_input("enter your age :"))
-    spy_name = spy_salutation + " " +spy_name+str(spy_rating)
-    print"alright" + spy_name +"I'd like to know more about you"
-    print "Welcome " + spy_name +" glad to have you back with us"
+# import statements
+from main import spy_name, spy_salutation, spy_age, spy_rating
+from start_chat import  start_chat
+
+print "Let's get started!"
+question = "Do you want to continue as " + spy_salutation + " " + spy_name + " (Y/N): "
+existing = raw_input(question)
+
+# validating users input
+if (existing == 'Y' or existing == 'y') :
+    # logic here.
+    start_chat(spy_name, spy_age, spy_rating)
+elif (existing == 'N' or existing == 'n'):
+    # new users code here.
+    spy_name = raw_input("Provide your name here :")
+    # chek whether spy has input something or not
+    if len(spy_name) > 0:
+        # code block if the condition is true.
+        # concatination of salutation and name.
+        spy_age = 0
+        spy_rating = 0.0
+        spy_is_online = False
+        spy_slautation = raw_input("What should we all you ? : ")
+        spy_age = raw_input("Enter your age. ?")
+        print type(spy_age)
+        spy_age = int(spy_age)
+        print type(spy_age)
+        spy_name = spy_slautation + " " + spy_name
+        print 'Welcome ' + spy_name + " Glad to have you back with us."
+        print "Alright " + spy_name + "i'd like know liitle bit more about you before we proceed further."
+    else:
+        print "Invalid name. Try again."
 else:
- print 'Invalid name Please try again'
+    print "Wrong choice. Try again."
