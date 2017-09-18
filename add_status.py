d384a684d4564224904c4aec39f9e8b6841e21ed
Charmi <charmisharma577@gmail.com>
2017-09-18 18:27:06 +0530

    bugs fixed

--- add_status.py
+++ add_status.py
@@ -1,65 +1,48 @@
-#IMPORT STATEMENT
+# import statements.
 from globals import STATUS_MESSAGES
-from main import spy
 from termcolor import colored
-#<-------------TO ADD STATUS------------->
-def add_status():
-    # in the beginning no status message
-    updated_status_message = None
 
-    # check if current status message is set or not
-    if spy.current_status_message is not None:
-        print 'Your current status message is %s \n' % spy.current_status_message
+# updated status message.
+updated_status_message = None
+def add_status(current_status_message):
+    if current_status_message != None:
+        print 'Your current status message is %s \n' % (current_status_message)
     else:
-        print 'You don\'t have any status message  \n'
+        print colored('You don\'t have any status message currently \n','red')
 
-    # Asking if the user wants to select a default status or a status which is already present
-    default = raw_input("Do you want to select from existing older status (y/n)?:- ")
+    # Ask user for choosing from older older messages.
+    default = raw_input("Do you want to select from the older status (y/n)? ")
 
-    # A spy wants to add another status rather from the existing one
-    # .upper() converts from any case to upper case
     if default.upper() == "N":
-        # ask the user to enter a new status
-        new_status_message = raw_input("What status message do you want to set?:- ")
+        new_status_message = raw_input("What status message do you want to set? ")
 
-        # if valid status message is entered
+        # validating users input.
         if len(new_status_message) > 0:
-            # in the existing status list add the new status
+            # adding new status message to the list.
             STATUS_MESSAGES.append(new_status_message)
-            # variable update
             updated_status_message = new_status_message
-
-    # A spy wants to choose from the existing status
+            print 'Your updated status message is: %s' % (updated_status_message)
+        else:
+            print colored("You did not provided any status message. Try again.",'red')
     elif default.upper() == 'Y':
-
-        # To give an index number to the statuses
+        # counter for serial number of messages.
         item_position = 1
 
-        # To show all the default statuses so that the user can select
+        # printing all older status messages.
         for message in STATUS_MESSAGES:
-            print (colored('%d. %s' % (item_position, message),'green'))
+            print '%d. %s' % (item_position, message)
             item_position = item_position + 1
 
-        # Ask the user which index of the list he wants to choose.
-        message_selection = int(raw_input("\nChoose the index of the status:- "))
+        # asking users choice.
+        message_selection = int(raw_input("\nChoose from the above messages "))
 
-        # Check if the position exists and then only set it
+        # validating users input.
         if len(STATUS_MESSAGES) >= message_selection:
-            # Variable update
             updated_status_message = STATUS_MESSAGES[message_selection - 1]
-
-    # When the user chooses neither yes nor no
+            print 'Your updated status message is: %s' % (updated_status_message)
+        else:
+            print colored("Invalid choice. Try again.",'red')
     else:
-        print (colored('The option you chose is not valid! Press either y or n.','red'))
+        print colored('The option you chose is not valid! Press either y or n.','red')
 
-    # When the status message is updated
-    if updated_status_message:
-        print (colored('Your updated status message is:','hello'))
-        print(colored(updated_status_message, "hello"))
-
-    # When it is not updated
-    else:
-        print(colored('You did not update your status message','Blue'))
-
-    # The updated message will be read
     return updated_status_message
